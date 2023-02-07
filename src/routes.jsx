646bc6e90b4770db3f9ad5f2c8bf89bac075f86f
Manu <65041975+ManuelaGripp@users.noreply.github.com>
2023-02-07 16:52:39 -0300

    inicio rotas

---
+++ src/routes.jsx
@@ -0,0 +1,12 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+
+const RoutesApp = ()=>{
+
+    <BrowserRouter>
+    <Routes>
+        <Router path='/login' element={<></>} />
+        <Router path='/editar' element={<></>} />
+    </Routes>
+    </BrowserRouter>
+
+}
